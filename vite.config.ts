96695971444b94e075bfa50bbb3cda202f70e5b1
leacloud <13883721874@163.com>
2022-12-05 00:19:04 +0800

    config: 主题定制

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,27 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import ElementPlus from "unplugin-element-plus/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+      },
+    },
+  },
   plugins: [
     vue(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({ importStyle: "sass" })],
+    }),
+    ElementPlus({
+      useSource: true,
     }),
   ],
   resolve: {
